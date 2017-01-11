23adc14e60d847a140e27c4a42a22021c74cb7bb
imininnik <evg.bakhmin@gmail.com>
2017-01-11 20:26:18 +0300

    joystick and movement
    Made joystick(need image for buttons and background) and basick movement
    system

---
+++ Assets/Scripts/PC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PC : MonoBehaviour {
+    Vector2 MovementDirection;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Transform>().Translate(MovementDirection * Speed, Space.World);
+	}
+    public void MoveVertical(int VerticalAxis)
+    {
+        MovementDirection.y = VerticalAxis;
+    }
+    public void MoveHorizontal(int HorizontalAxis)
+    {
+        MovementDirection.x = HorizontalAxis;
+    }
+}
